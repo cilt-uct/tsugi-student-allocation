5d1b58f80203322292c11361bea6a107fa9180ea
Charles Severance <csev@umich.edu>
2017-05-11 10:33:28 +0200

    Initial commit.

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Selector",
+"FontAwesome" => "fa-question-circle",
+"short_name" => "Selector",
+"description" => "Let students record their preferences for sections.",
+"messages" => array("launch")
+);
+
