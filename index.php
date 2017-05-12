a14179f41a2706ac2d62c6d25d737bfaeb02563c
TurRil <lightscape2001@gmail.com>
2017-05-12 12:52:55 +0200

    Updated selection with min and max constraints.

--- index.php
+++ index.php
@@ -37,38 +37,48 @@ if ( $USER->instructor ) {
     </form>
 <?php
 }
-//var_dump($user_json);
 
 $selection = json_decode($user_json);
 if (json_last_error() <> JSON_ERROR_NONE) {
-    print "error";
+    // print json_last_error_msg();
     $user_json = '{"result": {},"selection": []}';
 }
-
 
 $assigned_name = '';
 if (isset($selection->{'result'}->{'name'})) {
     $assigned_name = $selection->{'result'}->{'name'};
 }
 
-$json = '{ "expiry": "2017-05-14T08:00:00Z",'
-            .'"groups": { "max": 3, "min": 2},'
-            .'"all": ['
+$settings = Settings::linkGet('json');
+$json = json_encode($settings);
+
+//var_Dump($settings);
+/*
+$json ='{ "expiry": "2017-05-14T08:00:00Z",'
+            .'"constraints": { "max": 3, "min": 2},'
+            .'"groups": ['
                 .'{"id": "room1", "name": "ARoom 1"}'
                 .',{"id": "room2", "name": "BRoom 2"}'
                 .',{"id": "room3", "name": "CRoom 3"}'
                 .',{"id": "room4", "name": "DRoom 4"}'
                 .',{"id": "room5", "name": "ERoom 5"}'
             .']}';
-
-$settings = json_decode($json);
-if (json_last_error() <> JSON_ERROR_NONE) {
-    print "error";
+*/
+$date_now = new DateTime();
+$date_expiry = new DateTime();
+if (isset($settings['expiry'])) {
+    $date_expiry = new DateTime($settings['expiry']);
 }
 
-$date_now = new DateTime();
-$date_expiry = new DateTime($settings->{'expiry'});
 $date_left = $date_expiry->getTimestamp() - $date_now->getTimestamp();
+
+if (json_last_error() <> JSON_ERROR_NONE) {
+    print "error<br/>";
+    print json_last_error_msg() . "<br>";
+
+    $json = '{ "expiry": "'. date("Y-m-d") .'T06:00:00Z","constraints": { "max": -1}, groups: []}';
+    $date_left = -1;
+}
 ?>
 <div id="iframe-dialog" title="Read Only Dialog" style="display: none;">
    <iframe name="iframe-frame" style="height:400px" id="iframe-frame"
@@ -89,7 +99,7 @@ $date_left = $date_expiry->getTimestamp() - $date_now->getTimestamp();
                 
                 <div class="row">
                     <div class="col-xs-8">
-                        <h4>My Choices <span id="option-user-title"></span></h4>
+                        <h4 id="option-user-title">My Choices</h4>
                     </div>
                     <div class="col-xs-4 text-right">
                         <button type="button" id="options-user-clear" class="btn">Clear</button>
@@ -115,11 +125,12 @@ $date_left = $date_expiry->getTimestamp() - $date_now->getTimestamp();
         </div>
 
         <hr/>
-        <div class="row">
-            <div class="col-xs-3">
+        <div class="row footer">
+            <div class="col-xs-5" style="height:50px">
                 <button id="option-submit" type="button" data-disabled="1" class="btn btn-success hidden">Submit</button>
+                <div id="options-valid"></div>
             </div>
-            <div id="option-notification" class="col-xs-9"></div>
+            <div id="option-notification" class="col-xs-7"></div>
         </div>
 <?php } else { ?>
         
@@ -129,7 +140,7 @@ $date_left = $date_expiry->getTimestamp() - $date_now->getTimestamp();
                 <h4>Assigned to: <?= $assigned_name ?></h4>
                 <div class="row">
                     <div class="col-xs-8">
-                        <h4>My Choices <span id="option-user-title"></span></h4>
+                        <h4 id="option-user-title">My Choices</h4>
                     </div>
                     <div class="col-xs-4 text-right"></div>
                 </div>
@@ -177,6 +188,7 @@ $OUTPUT->footerStart();
 
             var selected = $('#options-user'),
                 selected_title = $('#option-user-title'),
+                selected_valid = $('#options-valid'),
                 available = $('#options-available'),
                 search_input = $('#options-search'),
                 search_id = null,
@@ -186,30 +198,35 @@ $OUTPUT->footerStart();
 
             function showSelected() {
                 var t = timer.data('time'),
-                    valid = (t > 1) && (my_selection.selection.length > 0) && (my_selection.selection.length == raw.groups.max);
-                selected.html(tmpl('tmpl-options-user', my_selection.selection));
-                selected_title.html('<span class="small">('+ my_selection.selection.length + (raw.groups.max > 0 ? ' of '+ raw.groups.max : '') +')</span>');
+                    valid = (t > 1) && (my_selection.selection.length > 0) &&
+                            ((my_selection.selection.length >= raw.constraints.min) || (my_selection.selection.length == raw.constraints.max));
+                selected.html(tmpl('tmpl-options-user', {list: my_selection.selection, limit: raw.constraints}));
+                selected_title.html(tmpl('tmpl-options-title', {len: my_selection.selection.length, limit: raw.constraints}));
+                selected_valid.html(tmpl('tmpl-options-valid', {len: my_selection.selection.length, limit: raw.constraints}));
 
                 if (valid) {
                     btn_submit.removeClass('disabled hidden').data('disabled', 0);
+                    selected_title.addClass('text-success').children('i').addClass('glyphicon-ok').removeClass('glyphicon-minus');
                 } else {
                    btn_submit.addClass('disabled').data('disabled', 1).removeClass('hidden');    
                    if (t < 1) {
                        btn_submit.addClass('hidden');
                    }
+
+                   selected_title.removeClass('text-success').children('i').addClass('glyphicon-minus').removeClass('glyphicon-ok');
                 }
             }
             
             function selectOption(_rel, _st){
-                var item = getOption(raw.all, _rel);   
+                var item = getOption(raw.groups, _rel);   
 
-                if (raw.groups.max === 0) {
+                if (raw.constraints.max === 0) {
                     $('#'+ _st + _rel).addClass('hidden');
                     item.order = my_selection.selection.length;
                     my_selection.selection.push(item);
                     showSelected();
                 } else {
-                    if (my_selection.selection.length < raw.groups.max) {
+                    if (my_selection.selection.length < raw.constraints.max) {
                         $('#'+ _st + _rel).addClass('hidden');
                         item.order = my_selection.selection.length;
                         my_selection.selection.push(item);
@@ -239,7 +256,7 @@ $OUTPUT->footerStart();
             showSelected();
             search_input.html('');
             if (available.length > 0) {
-                available.html(tmpl('tmpl-options-selectable', {list: raw.all, used: my_selection.selection}));
+                available.html(tmpl('tmpl-options-selectable', {list: raw.groups, used: my_selection.selection}));
                 selected.height(available.height());
                 available.height(available.height());
             }
@@ -278,7 +295,7 @@ $OUTPUT->footerStart();
                             tmp.push(item);
                         });
                         my_selection.selection = tmp;
-                        selected.html(tmpl('tmpl-options-user', my_selection.selection));
+                        selected.html(tmpl('tmpl-options-user', {list: my_selection.selection, limit: raw.constraints}));
                     }
                 });
             }
@@ -374,7 +391,7 @@ $OUTPUT->footerStart();
 
     <script type="text/x-tmpl" id="tmpl-options-user">
     {% 
-        o.sort(function(a, b) {
+        o.list.sort(function(a, b) {
             if (a.order > b.order) {
                 return 1;
             }
@@ -384,17 +401,39 @@ $OUTPUT->footerStart();
             return 0;
         });
 
-        if (o.length > 0) {
-        for (var i=0, len = o.length; i < len; i++) { 
-				var item = o[i]; %}
+        if (o.list.length > 0) {
+        for (var i=0, len = o.list.length; i < len; i++) { 
+				var item = o.list[i]; %}
         <div id="option-use-{%=item.id%}" data-id="{%=item.id%}"data-name="{%=item.name%}" data-order="{%=item.order%}">
 
             <a href="#" rel="{%=item.id%}" class="glyphicon glyphicon-remove"></a>
             <strong>{%=(i+1)%}<sup>{%=(num[i]?num[i]:'th')%}</sup></strong>&nbsp;&nbsp;{%=item.name%}
         </div>
-        {% } } else { %}<span>Please select your options before submission.</span>{% } %}
+        {% } } else { %}
+            {% if (o.limit.max != 0) { %}
+                {% if (o.limit.max == o.limit.min) { %}
+                    <span>Please select <strong>{%=o.limit.max%}</strong> option{%=(o.limit.max > 1?'s':'')%}.</span>
+                {% } else { %}
+                    <span>Please select at least <strong>{%=o.limit.min%}</strong> option{%=(o.limit.min > 1?'s':'')%}, maximum of <strong>{%=o.limit.max%}</strong>.</span>
+                {% } %}
+            {% } else { %}
+                <span>Please select your options.</span>
+            {% } %}
+        {% } %}
     </script>
 
+    <script type="text/x-tmpl" id="tmpl-options-valid">
+        <span class="small">
+        {% if (o.limit.max != 0) { %}
+            <br/>
+            {% if (o.limit.max == o.limit.min) { %}
+                (selected {%=o.len%} of {%=o.limit.max%})
+            {% } else { %}
+                (selected {%=o.len%} of {%=o.limit.max%}, at least {%=o.limit.min%})
+            {% } %}
+        {% } %}
+        </span>
+    </script>
 <?php } else { ?>
     <script type="text/x-tmpl" id="tmpl-options-user">
     {% 
@@ -408,14 +447,15 @@ $OUTPUT->footerStart();
             return 0;
         });
 
-        if (o.length > 0) {
-        for (var i=0, len = o.length; i < len; i++) { 
-				var item = o[i]; %}
+        if (o.list.length > 0) {
+        for (var i=0, len = o.list.length; i < len; i++) { 
+				var item = o.list[i]; %}
         <div id="option-use-{%=item.id%}" data-id="{%=item.id%}"data-name="{%=item.name%}" data-order="{%=item.order%}">
             <strong>{%=(i+1)%}<sup>{%=(num[i]?num[i]:'th')%}</sup></strong>&nbsp;&nbsp;{%=item.name%}
         </div>
-        {% } } else { %}<span>Please select your options before submission.</span>{% } %}
+        {% } } else { %}<span>The selection process is closed, please discuss this with your lecturer.</span>{% } %}
     </script>
+
 <?php } ?>
 
     <script type="text/x-tmpl" id="tmpl-timer">
@@ -447,6 +487,11 @@ $OUTPUT->footerStart();
     <script type="text/x-tmpl" id="tmpl-notify">
         <div class="alert alert-{%=o.type%}">{%#o.msg%}</div>
     </script>
+
+    <script type="text/x-tmpl" id="tmpl-options-title">
+        <i class="glyphicon"></i> My Choices <span class="small">({%=o.len%})</span>
+    </script>    
+
 <?php
 $OUTPUT->footerEnd();
 
